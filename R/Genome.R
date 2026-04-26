6a642eef7d8b0cfbaf7a12d8c78bba9fb40975fa
Ghaliya Alkamali <llghalyaa@gmail.com>
2026-04-26 14:40:56 +0400

    Update Genome.R

--- R/Genome.R
+++ R/Genome.R
@@ -1,6 +1,3 @@
-#MT sub
-#Ghaliya Alkamali 26 April 2025
-#Q1 & Q2
 setClass("Genome",slots= list(genome_name= "character", num_chromosomes= "numeric",num_proteins= "numeric"))
 setGeneric("avgProteinPerCh", function(object){
   standardGeneric("avgProteinPerCh")
