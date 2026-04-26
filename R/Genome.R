bdf7bc28f49a0bee249bc58a978fdca77e9a2e29
Ghaliya Alkamali <llghalyaa@gmail.com>
2026-04-26 14:18:28 +0400

    Create Genome class with average protein method

---
+++ R/Genome.R
@@ -0,0 +1,15 @@
+#MT sub
+#Ghaliya Alkamali 26 April 2025
+#Q1 & Q2
+setClass("Genome",slots= list(genome_name= "character", num_chromosomes= "numeric",num_proteins= "numeric"))
+setGeneric("avgProteinPerCh", function(object){
+  standardGeneric("avgProteinPerCh")
+})
+
+setMethod("avgProteinPerCh", "Genome",function(object){
+    avg= object@num_proteins/object@num_chromosomes
+    return(round(avg))
+  })
+library(GhaliyaGenomePackage)
+gen1=new("Genome", genome_name= "Human", num_chromosomes= 23, num_proteins= 20000)
+avgProteinPerCh(gen1)
